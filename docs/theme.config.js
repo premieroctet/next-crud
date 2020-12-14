a35ddd3f5e7c9a5f0f8defdb5c8ca607917b6d2a
foyarash <hfoyart@premieroctet.com>
2020-12-14 10:21:01 +0100

    Update documentation

---
+++ docs/theme.config.js
@@ -0,0 +1,22 @@
+export default {
+  repository: 'https://github.com/premieroctet/next-crud',
+  branch: 'master', // branch of docs
+  path: 'docs', // path of docs
+  titleSuffix: ' – next-crud',
+  nextLinks: true,
+  prevLinks: true,
+  search: true,
+  customSearch: null, // customizable, you can use algolia for example
+  darkMode: true,
+  footer: true,
+  footerText: 'MIT 2020 © Premier Octet.',
+  footerEditOnGitHubLink: true, // will link to the docs repo
+  logo: <span style={{ fontWeight: 'bold' }}>Next Crud</span>,
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="description" content="next-crud: a next.js API CRUD helper" />
+      <meta name="og:title" content="next-crud: a next.js API CRUD helper" />
+    </>
+  ),
+}
