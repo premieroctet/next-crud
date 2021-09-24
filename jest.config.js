0a36be42b4ed3d2a31aadfd051cca488ec8253fe
foyarash <hfoyart@premieroctet.com>
2021-09-24 14:40:27 +0200

    Add tests for the prisma adapter

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  clearMocks: true,
   preset: 'ts-jest',
   testEnvironment: 'node',
-};
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '<rootDir>/__tests__/adapters/prisma/client.ts',
+    '<rootDir>/__tests__/adapters/prisma/seed.ts',
+  ],
+}
