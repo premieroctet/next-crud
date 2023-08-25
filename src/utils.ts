35479cae530a8686cb2b0a1c76372e09c84d5b44
Ratul Maharaj <ratulmaharaj@gmail.com>
2023-08-25 11:25:36 +0200

    Add lowercase util

--- src/utils.ts
+++ src/utils.ts
@@ -224,3 +224,11 @@ export const getAccessibleRoutes = (
 
   return accessibleRoutes
 }
+
+/**
+ * Converts the first character of a word to lower case
+ * @param name
+ */
+export function lowerCase(name: string): string {
+  return name.substring(0, 1).toLowerCase() + name.substring(1)
+}
